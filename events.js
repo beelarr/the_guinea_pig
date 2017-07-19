46b3ef217e0e399f9733f5c32273a5fc389cfcd2
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-19 09:20:52 -0500

    Through #1

--- events.js
+++ events.js
@@ -1,3 +1,33 @@
 /**
  * Created by beelarr on 7/18/17.
  */
+
+
+let section_article = document.querySelectorAll('.article-section');
+// console.log(section_article)
+let output = document.querySelector('#output-target');
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (let i =0; i < section_article.length; i++){
+    section_article[i].addEventListener('click', function () {
+       let section_content = section_article[i].textContent;
+
+      output.innerHTML = `You clicked on the ${section_content} section`
+
+    });
+
+}
+
+
+
