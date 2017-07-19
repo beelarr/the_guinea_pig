dbe0d17102dc0480444d486b33d474feac2583ff
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-19 09:31:10 -0500

    Mousing over the header works

--- events.js
+++ events.js
@@ -22,12 +22,11 @@ let output = document.querySelector('#output-target');
 for (let i =0; i < section_article.length; i++){
     section_article[i].addEventListener('click', function () {
        let section_content = section_article[i].textContent;
-
-      output.innerHTML = `You clicked on the ${section_content} section`
-
+       output.innerHTML = `You clicked on the ${section_content} section`
     });
-
 }
 
 
-
+document.querySelector('h1').addEventListener('mouseover', function () {
+    output.innerHTML = `You moved your mouse over the header`
+});
